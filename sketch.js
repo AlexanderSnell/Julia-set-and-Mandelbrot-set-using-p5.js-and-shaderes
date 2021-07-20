ee3bd442281cef1d30b0505975e3443c37017a9a
This-is-excellent <51082734+This-is-excellent@users.noreply.github.com>
2021-07-20 19:19:06 +1000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,6 +12,21 @@ let scale = [scaler*(x/y),scaler];
 let spaceBar = false;
 let toggel = false;
 let c = [2,0];
+let touchScreenUser = null;
+
+// User type check
+try {
+  window.addEventListener('touchstart', function() {
+    if (touchScreenUser == null) {
+      let input = confirm("Press ok if you are using a mobile devices otherwise just press cancel.");
+      touchScreenUser = input;
+    }
+  });
+}
+catch(err) {
+  touchScreenUser = false;
+}
+
 
 function preload() {
   mandelbrot = loadShader('shader.vert', 'mandelbrot.frag');
@@ -56,6 +71,7 @@ function keyTyped() {
 }
 
 function mouseClicked() {
+  if (touchScreenUser == true) return;
   center[0] -= map(mouseX, 0, width, -scale[0], scale[0]);
   center[1] += map(mouseY, 0, height, -scale[1], scale[1]);
   scale[0] *= 0.6;
